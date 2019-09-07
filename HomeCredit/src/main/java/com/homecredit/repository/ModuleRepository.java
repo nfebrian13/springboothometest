06d2e5cc223aad9b109d0ac7b6763379873b8f4a
nfebrian13 <acer@DESKTOP-U7I9Q7E>
2019-09-08 01:45:12 +0700

    commit

--- HomeCredit/src/main/java/com/homecredit/repository/ModuleRepository.java
+++ HomeCredit/src/main/java/com/homecredit/repository/ModuleRepository.java
@@ -17,7 +17,7 @@ import org.springframework.data.repository.query.Param;
 public interface ModuleRepository extends JpaRepository<Module, Integer> {
 
 	@Query("SELECT new com.homecredit.dto.ModuleDto(d.moduleName, d.moduleOrder) "
-			+ "FROM Module d LEFT JOIN d.userDetail e where e.id = :id")
+			+ "FROM Module d LEFT JOIN d.userDetail e where e.id = :id order by d.moduleOrder asc")
 	List<ModuleDto> fetchModuleListByUserDetailId(@Param("id") Long id);
 
 }
